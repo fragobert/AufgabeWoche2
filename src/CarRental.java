398fe5473c65a6addfe8bffc8436ecddbddd1350
Fragobert <staflerflorian@gmail.com>
2022-09-25 23:13:07 +0200

    car objects can be rented

--- src/CarRental.java
+++ src/CarRental.java
@@ -4,8 +4,8 @@ public class CarRental {
     private static final String[] brands = {"BMW", "Mercedes", "Audi", "VW", "Opel", "Ford", "Fiat", "Renault", "Peugeot", "Citroen"};
     private static final String[] names = {"M3", "A45", "RS6", "Golf GTI", "Astra", "Focus", "500", "Clio", "308", "C4"};
     private static final String[] colors = {"rot", "blau", "schwarz", "weiß", "grün", "gelb", "orange", "violett", "pink", "braun"};
-    private ArrayList<Car> cars = new ArrayList<>();
-    private ArrayList<Car> rentedCars = new ArrayList<>();
+    private final ArrayList<Car> cars = new ArrayList<>();
+    private final ArrayList<Car> rentedCars = new ArrayList<>();
 
     public CarRental(int arraySize) {
         if (arraySize < 0) {
@@ -30,7 +30,7 @@ public class CarRental {
     }
 
     public void rentCar(int index) {
-        if (index < cars.size() && index >= 0) {
+        if (index < cars.size() && index > 0) {
             if (isAvailable(cars.get(index-1))) {
                 rentedCars.add(cars.get(index-1));
                 System.out.println("Das Auto wurde erfolgreich ausgeliehen!");
@@ -41,21 +41,22 @@ public class CarRental {
 
     }
 
-    public boolean returnCar(Car car) {
-        if (rentedCars.contains(car)) {
-            rentedCars.remove(car);
-            return true;
-        } else return false;
+    public void returnCar(int index) {
+        if(index <= 0 || index > cars.size()) {
+            System.out.println("Das Auto existiert nicht!");
+        }else{
+            if(rentedCars.contains(cars.get(index-1))){
+                rentedCars.remove(cars.get(index-1));
+                System.out.println("Das Auto wurde erfolgreich zurueckgegeben!");
+            }else{
+                System.out.println("Das Auto wurde nicht ausgeliehen!");
+            }
+        }
     }
 
     private static String stringFormatter(String string) {
         return String.format("%1$" + 10 + "s", string);
     }
-
-    private static String stringFormatter(String string, int length) {
-        return String.format("%1$" + 10 + "s", string);
-    }
-
     public void getCars() {
         if (cars.size() == 0) {
             System.out.println("Es sind keine Autos vorhanden!");
